075d36ca5d6eb6c495019731d06c1c0408526097
Minseo Jeong <jeongminseo999@gmail.com>
2022-01-06 16:44:29 +0900

    input으로 타이머시간 받아오기_최종

---
+++ 220106/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Dday from './Dday';
+
+
+function App() {
+  const [inputs, setInputs] = useState({
+    hours : "",
+    minutes : "",
+    seconds : ""
+  });
+
+  const { hours, minutes, seconds } = inputs; 
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setInputs({
+        ...inputs,
+        [name]:value
+    });
+  };
+
+  return (
+    <div>
+      <input name="hours" placeholder="시" onChange={onChange} value={hours}/>
+      <input name="minutes" placeholder="분" onChange={onChange} value={minutes}/>
+      <input name="seconds" placeholder="초" onChange={onChange} value={seconds}/>
+      <Dday hours={hours} minutes={minutes} seconds={seconds} setInputs={setInputs}/>  
+    </div>
+  );
+}
+
+export default App;
