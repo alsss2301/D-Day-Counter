4bdd19fa5556d0f86674fc99ca2751722be7fd7c
Minseo Jeong <jeongminseo999@gmail.com>
2022-01-10 23:04:13 +0900

    input스타일링

---
+++ 220110_2/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import styles from './App.module.css';
+import Dday from './Dday';
+
+
+function App() {
+  const [inputs, setInputs] = useState({
+    hours : "",
+    minutes : "",
+    seconds : ""
+  });
+
+  const { hours, minutes, seconds } = inputs; 
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setInputs({
+        ...inputs,
+        [name]:value
+    });
+  };
+
+  return (
+      <div>
+      <div className = {styles.Block}>
+        <input className = {styles.Input} type="text" name="hours" placeholder="시간" onChange={onChange} value={hours}/>
+        <input className = {styles.Input} type="text" name="minutes" placeholder="분" onChange={onChange} value={minutes}/>
+        <input className = {styles.Input} type="text" name="seconds" placeholder="초" onChange={onChange} value={seconds}/>
+      <Dday hours={hours} minutes={minutes} seconds={seconds} setInputs={setInputs}/>  
+      </div>
+      </div>
+  );
+}
+
+export default App;
