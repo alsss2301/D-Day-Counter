0ed0f8cc8be3d8bcad1da7d6b91afb0f346238c1
Minseo Jeong <jeongminseo999@gmail.com>
2022-01-11 16:43:39 +0900

    지정된날짜넣은카운터코드

---
+++ 220111-2/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dday from './Dday';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className = "App">
+        <Dday />  
+    </div>
+  );
+}
+
+export default App;
