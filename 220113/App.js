ba14c4e19fbfdda3e9ce2d6934916421eddf6d5d
Minseo Jeong <jeongminseo999@gmail.com>
2022-01-13 18:08:40 +0900

    Input날짜받아오기,CSS

---
+++ 220113/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import Dday from './Dday';
+import styles from './App.module.css';
+
+function App() {
+
+  return (
+    <div className = {styles.App}>
+      <Dday />  
+    </div>
+  );
+}
+
+export default App;
